95c83852ce7641933985d1d79eb421aed89d0e3e
learngermanghana <learngermanghana@gmail.com>
2026-05-17 17:11:36 +0000

    Fix charity form routes for Sedifex success pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import TransparencyPage from "./pages/TransparencyPage";
 import VolunteerPage from "./pages/VolunteerPage";
 import RequestSupportPage from "./pages/RequestSupportPage";
 import BlogPage from "./pages/BlogPage";
+import SuccessPage from "./pages/SuccessPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
@@ -22,9 +23,14 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/photos" element={<GalleryPage />} />
           <Route path="/get-involved" element={<GetInvolvedPage />} />
+          <Route path="/donate" element={<Navigate to="/get-involved" replace />} />
+          <Route path="/donation-success" element={<SuccessPage kind="donation" />} />
           <Route path="/transparency" element={<TransparencyPage />} />
           <Route path="/volunteer" element={<VolunteerPage />} />
+          <Route path="/volunteer-success" element={<SuccessPage kind="volunteer" />} />
           <Route path="/request-support" element={<RequestSupportPage />} />
+          <Route path="/request" element={<Navigate to="/request-support" replace />} />
+          <Route path="/request-success" element={<SuccessPage kind="request" />} />
           <Route path="/inspiring-stories" element={<BlogPage />} />
           <Route path="/blog" element={<Navigate to="/inspiring-stories" replace />} />
           {/* fallback */}
